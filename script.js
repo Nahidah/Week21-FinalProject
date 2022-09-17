169aa298f8dcc89b75707e66bc8f7c0209dfcd9c
damien-mallet <damien.mallet.91@gmail.com>
2022-09-17 09:43:43 +0400

    Fixed EJS

---
+++ script.js
@@ -0,0 +1,107 @@
+//alert("Loading");//
+function addNewWEField() {
+    console.log("Adding New page");
+ 
+    let newNode=document.createElement('textarea');
+     newNode.classList.add('form-control');
+     newNode.classList.add('weField');
+     newNode.classList.add('mt-2');
+     newNode.setAttribute('rows',3);
+     newNode.setAttribute('placeholder','Enter here')
+ 
+     let weOb=document.getElementById('we');
+     let weAddButtonOb=document.getElementById('weAddButton');
+ 
+     weOb.insertBefore(newNode, weAddButtonOb);
+ }
+ 
+ function addNewAQField() {
+     let newNode=document.createElement('textarea');
+     newNode.classList.add('form-control');
+     newNode.classList.add('eqField');
+     newNode.classList.add('mt-2');
+     newNode.setAttribute('rows',3);
+     newNode.setAttribute('placeholder','Enter here')
+ 
+     let aqOb=document.getElementById('aq');
+     let aqAddButtonOb=document.getElementById('aqAddButton');
+ 
+     aqOb.insertBefore(newNode, aqAddButtonOb);
+ }
+ 
+ //generating cv
+ function generate(){
+     //generating cv
+     let nameField=document.getElementById("nameField").value;
+ 
+     let nameT1=document.getElementById("nameT1");
+ 
+     nameT1.innerHTML = nameField;
+
+     let nameField2=document.getElementById("nameField1").value;
+ 
+     let nameT2=document.getElementById("nameT2");
+ 
+     nameT1.innerHTML = nameField;
+ 
+     //direct
+     document.getElementById("nameT2").innerHTML = nameField;
+ 
+     //contact
+     document.getElementById("contactT").innerHTML =document.getElementById("contactField").value;
+ 
+     //address
+     document.getElementById("addressT").innerHTML = document.getElementbyId("addressfield").value;
+     document.getElementById("fb").innerHTML = document.getElementbyId("fbfield").value;
+     document.getElementById("instaT").innerHTML = document.getElementbyId("instafield").value;
+ 
+     //objective
+     objective.getElementById("objectiveT").innerHTML=document.getElementById("objectField").value;
+ 
+     //work experience
+     let wes=document.getElementByClassName("weField")
+ 
+     let str=''
+ 
+     for(let e of wes)
+     {
+         str=str+`<li> ${e.value} </li`;
+     }
+ 
+     document.getElementById("workExperience").innerHTML =str;
+ 
+     //aq
+     let aqs=document.getElementsByClassName('aqField')
+ 
+     let str1=''
+ 
+     for(let e of aqs) {
+         str1 += `<li> ${e.value} </li>`;
+     }
+ 
+     document.getElementById('aqT').innerHTML=str1;
+ 
+     //uploading image
+     let file=document.getElementById("imgField").files[0]
+     console.log(file);
+ 
+     let reader=new FileReader();
+ 
+     reader.readAsDataURL(file);
+     console.log(reader.result);
+ 
+     //setting image to template
+ 
+     reader.onloadend = function() {  
+         document.getElementById("imgTemplate").src = reader.result;
+     };
+ 
+ 
+     document.getElementById("cv-form").style.display="none";
+     document.getElementById("cv-template").style.display= "block";
+ 
+     //printing resume
+     function printCV(){
+         window.print();
+     }
+ }
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express=require('express');
+const {reset} = require('nodemon');
+
 const app= express();
+
+const PORT = process.env.PORT || 3000;
 
 //set the view engine to ejs
 //set ejs as view engine for express app
 app.set('view engine','ejs');
+app.use(express.static('public'))
 
 app.get('/', (req,res)=> {
    
     res.render('home'); //renders the home.ejs file
 })
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
-//NEW ROUTE
-app.get('/',(req,res) => {
-    res.render('css');
+app.get('/resume', (req,res)=> {
+
+    res.render('resume'); //renders the home.ejs file
 })
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+//
+// //NEW ROUTE
+// app.get('/',(req,res) => {
+//     res.render('css');
+// })
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
